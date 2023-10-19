1c27e14c615d38d6262e395edf5e5057e2f6980d
abuzar <gafariabuzar.2@gmail.com>
2023-10-19 14:08:08 +0700

    Update File

--- myscript.js
+++ myscript.js
@@ -1,17 +1,15 @@
-const dropdown = document.querySelector('.dropdown');
 const menuDua = document.getElementById('menu-kedua');
 const menuTiga = document.getElementById('menu-ketiga');
 const icontog = document.getElementById('icontog');
 const toggler = document.getElementById('toggler');
 
 
-toggler.addEventListener('click',()=>{
-    if(toggler.checked==true){
+toggler.addEventListener('click', () => {
+    if (toggler.checked == true) {
         icontog.classList.remove('fa-bars');
         icontog.classList.add('fa-xmark');
-    }
-    else{
-        if(toggler.checked==false){
+    } else {
+        if (toggler.checked == false) {
             icontog.classList.remove('fa-xmark');
             icontog.classList.add('fa-bars');
         }
@@ -19,18 +17,16 @@ toggler.addEventListener('click',()=>{
 })
 
 
-
 document.addEventListener('click', (e) => {
-  let elementClass = e.target.className;
-  if (
-    elementClass !== 'left-icon' &&
-    elementClass !== 'right-icon' &&
-    elementClass !== 'text'
-  ) {
-    menuDua.classList.remove('active');
-    menuTiga.classList.remove('active');
-  } else {
-  }
+    let elementClass = e.target.className;
+    if (
+        elementClass !== 'left-icon' &&
+        elementClass !== 'right-icon' &&
+        elementClass !== 'text'
+    ) {
+        menuDua.classList.remove('active');
+        menuTiga.classList.remove('active');
+    } else {}
 });
 
 menuDua.addEventListener('click', () => {
@@ -42,4 +38,3 @@ menuTiga.addEventListener('click', () => {
     menuTiga.classList.toggle('active');
     menuDua.classList.remove('active');
 });
-
